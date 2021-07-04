606e1c6c26f63f015979b8917f1274529cf84bae
quboka <1362420246@qq.com>
2021-07-04 10:37:22 +0800

    消息过滤器

--- mq/spring-kafka-demo/src/main/java/com/qbk/kafka/demo/KafkaDemoApplication.java
+++ mq/spring-kafka-demo/src/main/java/com/qbk/kafka/demo/KafkaDemoApplication.java
@@ -4,14 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * 生产者:
  * 1.简单发送消息
  * 2.带回调的发送消息
  * 3.发送事务消息
  * 4.自定义分区器
  *
+ * 消费者:
  * 1.简单消费
  * 2.指定topic、partition、offset消费
  * 3.批量消费
+ * 4.消费异常处理器
+ * 5.消息过滤器
  */
 @SpringBootApplication
 public class KafkaDemoApplication {
